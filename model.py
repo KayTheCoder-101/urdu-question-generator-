37dfe90828a85a115c20f093b91825bf3f5fb14c
KayTheCoder-101 <kulsoomume362@gmail.com>
2026-09-10 13:59:21 +0500

    Simplify Encoder bridging: average bidirectional states instead of concat+Linear

--- model.py
+++ model.py
@@ -6,20 +6,16 @@ class encoder (nn.Module):
         super().__init__()
         self.embedding=nn.Embedding(v_size,e_size)
         self.rnn=nn.LSTM(e_size,h_size,layers,bidirectional=True,dropout=dout,batch_first=True)
-        self.hidden=nn.Linear(h_size*2,h_size)
-        self.cell=nn.Linear(h_size*2,h_size)
+
     
     def forward(self,s):
         Embeddings=self.embedding(s)
         outputs,(hidden,cell)=self.rnn(Embeddings)
         hiddenStates=hidden[0::2,:,:]
         BhiddenStates=hidden[1::2,:,:]
-        combining_hidden_states=torch.cat([hiddenStates,BhiddenStates],dim=2) #hr layer ka forward aur vector ab ik sath ik hi layer mai pry hain
-        output_hidden=self.hidden(combining_hidden_states)
+        combining_hidden_states=(hiddenStates + BhiddenStates) / 2
         cellStates=cell[0::2,:,:]
         BcellStates=cell[1::2,:,:]
-        combining_cell_states=torch.cat([cellStates,BcellStates],dim=2)
-        output_cell=self.cell(combining_cell_states)
+        combining_cell_states=(cellStates + BcellStates) / 2
 
-        return outputs,output_hidden,output_cell
-
+        return outputs, combining_hidden_states, combining_cell_states
