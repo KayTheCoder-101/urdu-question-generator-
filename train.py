fdc7a17bab4130ed94f0899a505ac2ad2d6023d3
KayTheCoder-101 <kulsoomume362@gmail.com>
2026-09-11 17:01:09 +0500

    Update training loop: 10 epochs, checkpoint saving

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from model import encoder,decoder
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 ds_train=QGDataset("data/train.tsv","tokenizer/ur_sp.model")
 ds_valid=QGDataset("data/valid.tsv","tokenizer/ur_sp.model")
@@ -24,7 +25,11 @@ Loss=nn.CrossEntropyLoss(ignore_index=0)
 
 optimizer=torch.optim.Adam(list(enc.parameters())+list(dec.parameters()),lr=0.001)
 
-e=5
+enc = enc.to(device)
+dec = dec.to(device)
+
+e=10
+best_val_loss=float('inf')
 
 for epoch in range(e):
     print("Epoch Number:",epoch+1)
@@ -33,6 +38,8 @@ for epoch in range(e):
     enc.train()
     dec.train()
     for src_batch,tgt_batch in train_loader:
+        src_batch = src_batch.to(device)
+        tgt_batch = tgt_batch.to(device)
         enc_out,h,c=enc(src_batch)
         outputs,att=dec(enc_out,h,c,tgt_batch)
         L=tgt_batch[:,1:].reshape(-1)
@@ -47,10 +54,17 @@ for epoch in range(e):
     dec.eval()
     with torch.no_grad(): 
         for src,tgt in valid_loader:
+            src = src.to(device)
+            tgt = tgt.to(device)
             enc_out,h,c=enc(src)
             outputs,att=dec(enc_out,h,c,tgt)
             L_v=tgt[:,1:].reshape(-1)
             for_out=outputs.reshape(-1,v_size)
             V_loss=Loss(for_out,L_v)
             v_loss_list.append(V_loss.item())
-        print("avg validation loss",sum(v_loss_list)/len(v_loss_list))
+        avg_val_loss=sum(v_loss_list)/len(v_loss_list)
+        print("avg validation loss",avg_val_loss)
+        if avg_val_loss<best_val_loss:
+            best_val_loss=avg_val_loss
+            torch.save({'encoder': enc.state_dict(), 'decoder': dec.state_dict()}, "best_model.pt")
+
